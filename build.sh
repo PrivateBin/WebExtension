15374778902b7c2bc358379f88ce1f039fc5e861
rugk <rugk@posteo.de>
2016-12-14 01:22:05 +0100

    Clean up code & add/extend dev/built files

--- build.sh
+++ build.sh
@@ -1,1 +1,21 @@
-7z a ~/privatebin.xpi *.js *.json
+#!/bin/sh
+# LICENSE: BSD 2-Clause License, see LICENSE.md
+#
+# Creates a build of an add-on release.
+#
+
+# variables
+CHANNEL='productive'
+VERSION='0.1'
+
+# parameter parsing
+for ARG in "$@"
+do
+	[ "$ARG" = "-d" ] || [ "$ARG" = "--dev" ] || [ "$ARG" = "--development" ] && CHANNEL='development'
+done
+
+# make dir if necessary
+mkdir -p ./build
+
+# compress 
+7z a "./build/privatebin_$CHANNEL\_$VERSION.xpi" *.js *.json
